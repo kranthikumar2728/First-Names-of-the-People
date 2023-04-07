578c2b0b438054888d87a9c6c6a9aeb521231d3d
kranthikumar2728 <kranthikumark839@gmail.com>
2023-04-07 10:27:21 +0000

    First Names of the People

---
+++ index.js
@@ -0,0 +1,8 @@
+let peopleNames = require("../country/state/city/index");
+let getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peopleNames) => {
+  return getFirstNames(peopleNames);
+};
+
+module.exports = getPeopleInCity;
